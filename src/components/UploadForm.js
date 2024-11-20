a92b70835f682dc89c394d963a9c22d35adf7276
hillary <sam.mukili10@gmail.com>
2024-11-20 20:41:42 +0300

    one problem file

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -8,7 +8,7 @@ import './UploadForm.css';
 // Initialize Supabase client
 const supabase = createClient(
   process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_ANON_KEY
+  process.env.REACT_APP_SUPABASE_KEY
 );
 
 // AWS Rekognition setup
